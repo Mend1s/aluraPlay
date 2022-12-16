f17c392f24d4a39bf5bf43c6828b373248fc4cb2
Mend1s <andremendesskt99@gmail.com>
2022-12-15 23:50:52 -0300

    ajustando metodo de busca

--- js/buscaVideo.js
+++ js/buscaVideo.js
@@ -1,11 +1,19 @@
 import { conectaApi } from "./conectarApi.js";
-import { mostrarVideos } from ""
+import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
     evento.preventDefault();
-    
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscarVideos(dadosDePesquisa);
+
+    const lista = document.querySelector("[data-lista]");
+
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 }
 
 const botaoPesquisa = document.querySelector("[data-botao-pesquisa]");
